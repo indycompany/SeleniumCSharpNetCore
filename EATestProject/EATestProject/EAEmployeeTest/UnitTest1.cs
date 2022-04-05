9a5ab2e91f5dfc141c88ea0248b87110b3de3420
Ivan Nadych <inadych@atlassian.com>
2022-04-05 15:09:20 +0300

    new files added

---
+++ EATestProject/EATestProject/EAEmployeeTest/UnitTest1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using EAEmployeeTest.Pages;
+using EAAutoFramework.Base;
+using OpenQA.Selenium.Chrome;
+using EAAutoFramework.Helpers;
+
+namespace EAEmployeeTest
+{
+    [TestClass]
+    public class UnitTest1 : Base
+    {
+
+
+        string url = "http://eaapp.somee.com/";
+
+        public void OpenBrowser(BrowserType browserType)
+        {
+            switch (browserType)
+            {
+                case BrowserType.FireFox:
+                    Console.WriteLine("Not implemented yet");
+                    break;
+                case BrowserType.Chrome:
+                    DriverContext.Driver = new ChromeDriver();
+                    DriverContext.Browser = new Browser(DriverContext.Driver);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string fileName = Environment.CurrentDirectory.ToString() + "\\Data\\Login.xlsx";
+
+            ExcelHelpers.PopulateInCollection(fileName);
+            LogHelpers.CreateLogFile();
+
+            OpenBrowser(BrowserType.Chrome);
+            LogHelpers.Write("Open the browser!!!");
+            DriverContext.Browser.GoToUrl(url);
+            LogHelpers.Write("Navigate to the URL!!!");
+            //LoginPage
+
+            CurrentPage = GetInstance<LoginPage>();
+            CurrentPage.As<LoginPage>().ClickLoginLink();
+            CurrentPage.As<LoginPage>().Login(ExcelHelpers.ReadData(1,"UserName"), ExcelHelpers.ReadData(1, "Password"));
+            //EmployeePage
+            CurrentPage = CurrentPage.As<LoginPage>().ClickEmployeeList();
+            CurrentPage.As<EmployeePage>().ClickCreateNew();
+
+
+        }
+    }
+}
